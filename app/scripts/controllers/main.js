258e0c5f076d70a22e9fe80bca32a15308561f14
alexkb <alex.bergin@gmail.com>
2015-01-23 15:17:31 +0800

    Attempt with ng-if functionality.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,10 +8,22 @@
  * Controller of the sdcApp
  */
 angular.module('Sdc')
-  .controller('MainCtrl', function ($scope, appData) {
-    $scope.data = appData;
+  .controller('MainCtrl', function ($scope, Geo) {
+    //$scope.data = appData;
+    $scope.data = {};
+
+    // Set default State on load based on geolocation service.
+    Geo.getLocation().then(function(position) {
+      var lat = position.coords.latitude;
+      var lng = position.coords.longitude;
+      Geo.reverseGeocode(lat, lng).then(function(locString) {
+        $scope.data.state = locString;
+      });
+    });
 
     $scope.calculateDutyDue = function() {
-      return appData.propertyValue + 10;
+      //console.log(appData);
+      //return appData.propertyValue + 10;
+
     };
   });
--- app/scripts/directives/calculator.js
+++ app/scripts/directives/calculator.js
@@ -7,19 +7,15 @@
  * # calculator
  */
 angular.module('Sdc')
-  .directive('calculator', function (Geo) {
+  .directive('calculator', function (appData) {
     return {
       restrict: 'E',
       link: function postLink($scope, element, attrs) {
-        Geo.getLocation().then(function(position) {
-          var lat = position.coords.latitude;
-          var lng = position.coords.longitude;
-          Geo.reverseGeocode(lat, lng).then(function(locString) {
-            console.log(locString);
-            $scope.locString = locString;
-          });
-        });
+
         console.log(attrs);
+        console.log($scope.data);
+        console.log(appData);
+        //$scopedata.dutyDue = 100;
         //console.log(appData);
         //appData = $scope.data;
         //$scope.data = appData;
