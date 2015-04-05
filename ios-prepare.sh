8ce508e33c36b43a9dc4d012e9a3888819ca824d
Alex Bergin <alex.bergin@gmail.com>
2015-04-05 19:22:07 +0800

    Removing border on terms note. Also tweaked ios prepare script to ensure relevant plugins are installed.

--- ios-prepare.sh
+++ ios-prepare.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-# This script runs a clean build of the app for me.. I dunno why the ios platform is so much more difficult to build for than android!
+# This script runs a clean build of the app for me.. I dunno why the ios platform is so much
+# more difficult to build for than android!
 
 ionic platform rm ios
 
@@ -8,8 +9,10 @@ rm -rf plugins/ platforms/ www/
 
 mkdir www
 
-cordova plugin add com.ionic.keyboard org.apache.cordova.device org.apache.cordova.statusbar org.apache.cordova.inappbrowser
-# org.apache.cordova.console
+cordova plugin add com.ionic.keyboard org.apache.cordova.device org.apache.cordova.statusbar \
+org.apache.cordova.inappbrowser org.apache.cordova.console org.apache.cordova.geolocation
+
+cordova plugin add https://github.com/katzer/cordova-plugin-email-composer.git
 
 ionic platform add ios
 
