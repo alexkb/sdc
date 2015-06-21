58727b805d5b47c0f633ddcfbf86147b3f4d75ce
Alex Bergin <alex.bergin@gmail.com>
2015-06-21 21:03:13 +0800

    Using new dev environment so taking out funky windows crap for window spawn and also saved a few more dependencies.

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@
 var _ = require('lodash');
 var path = require('path');
 var cordovaCli = require('cordova');
-var spawn = require('win-spawn');
+var spawn = require('child_process').spawn;
 
 module.exports = function (grunt) {
 
